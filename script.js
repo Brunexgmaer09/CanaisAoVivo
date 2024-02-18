8110b4a24900a320b239ae9def1f43ed340b3c56
Brunexgmaer09 <39810796+Brunexgmaer09@users.noreply.github.com>
2024-02-18 17:25:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,43 +12,46 @@ iframeVideo.src = `https://reidoscanais.com/embed/?id=${idVideoPadrao}`;
 body.style.backgroundColor = '#000';
 
 listaAmbientes.forEach((button) => {
-  button.addEventListener('click', () => {
-    const idIframe = button.getAttribute('data-id');
+  button.addEventListener('click', () => {
+    const idIframe = button.getAttribute('data-id');
 
-    // Se for a Rede Globo, use a URL específica
-    if (idIframe === 'globomg-globominas') {
-      iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+    // Se for a Rede Globo, use a URL específica
+    if (idIframe === 'globomg-globominas') {
+      iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
     } else if (idIframe === 'discoverychannel') { // Condição para o Discovery HD
         iframeVideo.src = 'https://reidoscanais.com/embed/?id=discoverychannel';
     } else {
-      // Use o código existente para outros botões
-      switch (idIframe) {
-        case 'bbb2':
-          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
-          break;
-        case 'bbb4':
-          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
-          break;
-        case 'bbb3':
-          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
-          break;
-        case 'bbb9':
-          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
-          break;
-        case 'bbb11':
-          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
-          break;
-        default:
-          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idVideoPadrao}`;
-      }
-    }
+      // Use o código existente para outros botões
+      switch (idIframe) {
+        case 'bbb2':
+          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+          break;
+        case 'bbb4':
+          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+          break;
+        case 'bbb3':
+          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+          break;
+        case 'bbb9':
+          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+          break;
+        case 'bbb11':
+          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+          break;
+        default:
+          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idVideoPadrao}`;
+      }
+    }
 
-    // Animação para mostrar o vídeo
-    iframeVideo.style.opacity = 0;
-    iframeVideo.style.display = "block";
-    iframeVideo.style.transition = "opacity 0.5s ease-in-out";
-    setTimeout(() => {
-      iframeVideo.style.opacity = 1;
-    }, 100);
-  });
+    // Animação para mostrar o vídeo
+    iframeVideo.style.opacity = 0;
+    iframeVideo.style.display = "block";
+    iframeVideo.style.transition = "opacity 0.5s ease-in-out";
+    setTimeout(() => {
+      iframeVideo.style.opacity = 1;
+    }, 100);
+  });
 });
+
+// Adicione o botão do mosaico do BBB24 ao final da lista
+listaAmbientes.push(document.querySelector('.ambiente[data-id="bbbmosaico"]'));
