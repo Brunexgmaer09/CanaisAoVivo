d761a93b489c1b540571970798864e5f2f463a1a
Brunexgmaer09 <39810796+Brunexgmaer09@users.noreply.github.com>
2024-03-29 12:47:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const listaAmbientes = document.querySelectorAll('.ambiente');
 const iframeVideo = document.getElementById('iframe-video');
 const body = document.querySelector('body');
 
-// Adicione os IDs da Rede Globo, Discovery HD e o novo "Jardim"
-const listaIds = ['bbb', 'bbb2', 'bbb4', 'bbb3', 'bbb9', 'bbb11', 'globomg-globominas', 'discoverychannel', 'bbbmosaico', 'bbb5'];
+// Adicione os IDs dos novos canais
+const listaIds = ['bbb', 'bbb2', 'bbb3', 'bbb4', 'bbb5', 'bbb6', 'bbb7', 'bbb8', 'bbb10', 'bbb11'];
 
-// Carregar o vídeo do "Quarto Roxo" por padrão
+// Carregar o "Acompanhe a Casa" por padrão
 iframeVideo.src = `https://reidoscanais.com/embed/?id=bbb`;
 
 body.style.backgroundColor = '#000';
@@ -14,46 +14,40 @@ listaAmbientes.forEach((button) => {
   button.addEventListener('click', () => {
     const idIframe = button.getAttribute('data-id');
 
-    // Se for a Rede Globo, use a URL específica
-    if (idIframe === 'globomg-globominas') {
-      iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
-    } else if (idIframe === 'discoverychannel') { // Condição para o Discovery HD
-        iframeVideo.src = 'https://reidoscanais.com/embed/?id=discoverychannel';
-    } else {
-      // Use o código existente para outros botões
-      switch (idIframe) {
-        case 'bbb2':
-          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
-          break;
-        case 'bbb4':
-          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
-          break;
-        case 'bbb3':
-          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
-          break;
-        case 'bbb9':
-          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
-          break;
-        case 'bbb11':
-          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
-          break;
-        case 'bbbmosaico':
-          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
-          break;
-        case 'jardim':
-          iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
-          break;
-        default:
-          iframeVideo.src = `https://reidoscanais.com/embed/?id=bbb`;
-      }
+    // Atualize a lógica para usar os novos IDs
+    switch (idIframe) {
+      case 'bbb':
+        iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+        break;
+      case 'bbb2':
+        iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+        break;
+      case 'bbb3':
+        iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+        break;
+      case 'bbb4':
+        iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+        break;
+      case 'bbb5':
+        iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+        break;
+      case 'bbb6':
+        iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+        break;
+      case 'bbb7':
+        iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+        break;
+      case 'bbb8':
+        iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+        break;
+      case 'bbb10':
+        iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+        break;
+      case 'bbb11':
+        iframeVideo.src = `https://reidoscanais.com/embed/?id=${idIframe}`;
+        break;
+      default:
+        console.log('ID do canal inválido:', idIframe);
     }
-
-    // Animação para mostrar o vídeo
-    iframeVideo.style.opacity = 0;
-    iframeVideo.style.display = "block";
-    iframeVideo.style.transition = "opacity 0.5s ease-in-out";
-    setTimeout(() => {
-      iframeVideo.style.opacity = 1;
-    }, 100);
   });
 });
